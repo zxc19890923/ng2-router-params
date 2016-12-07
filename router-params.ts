25f446c8ea5f33d9221d6c826a2975e37ad1db85
zhangxuchao <2212362272@qq.com>
2016-12-07 17:22:41 +0800

    Create router-params.ts

---
+++ router-params.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from "@angular/core";
+// 获取路由传递传递过来的params(id) 增加模块激活的路由(ActivatedRoute)
+import {ActivatedRoute} from "@angular/router";
+@Component({
+    selector: "my-info",
+    templateUrl: "../templates/about-info.html"
+})
+
+export class AboutInfoComponent implements OnInit{
+    // 初始化变量
+    constructor(public route: ActivatedRoute) {
+    }
+    // 方法中操作id, 通过id查询信息等等
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            console.log(params); //{ id : "xx" }
+        });
+    }
+}
